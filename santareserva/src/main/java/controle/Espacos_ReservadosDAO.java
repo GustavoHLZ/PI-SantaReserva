bc2504ff375f64e3ca2cc1e8e0d72b6ddd3db96e
MHBranco <murilo.b08@aluno.ifsc.edu.br>
2024-02-22 14:27:13 -0300

    Commit InterfaceDAO2

--- santareserva/src/main/java/controle/Espacos_ReservadosDAO.java
+++ santareserva/src/main/java/controle/Espacos_ReservadosDAO.java
@@ -1,20 +1,40 @@
 package controle;
 
+import java.util.ArrayList;
+
 import modelo.Espacos_Reservados;
 
-public class Espacos_ReservadosDAO {
+public class Espacos_ReservadosDAO implements IEspacos_ReservadosDAO {
 
-	public Espacos_ReservadosDAO() {
+	@Override
+	public int inserirEspacos_Reservados(Espacos_Reservados end) {
 		
+		return 0;
 	}
+
+	@Override
+	public ArrayList<Espacos_Reservados> listarEspacos_Reservados() {
+		
+		return null;
+	}
+
+	@Override
+	public boolean atualizarEspacos_Reservados(Espacos_Reservados end) {
+		
+		return false;
+	}
+
+	@Override
+	public boolean removerEspacos_Reservados(Espacos_Reservados end) {
+		
+		return false;
+	}
+
+	@Override
+	public Espacos_Reservados buscarEspacos_Reservados(Espacos_Reservados end) {
+		
+		return null;
+	}
+
 	
-	public Boolean Inserir(Espacos_Reservados a) {
-		return true;
-	}
-	public Boolean Alterar(Espacos_Reservados a) {
-		return true;
-	}
-	public Boolean Excluir(Espacos_Reservados a) {
-		return true;
-	}
 }
---
+++ santareserva/src/main/java/controle/IComputadoresDAO.java
@@ -0,0 +1,19 @@
+package controle;
+
+import java.util.ArrayList;
+
+import modelo.Computadores;
+
+public interface IComputadoresDAO {
+
+	public int inserirComputadores(Computadores end);
+	
+	public ArrayList<Computadores> listarComputadores();
+	
+	public boolean atualizarComputadores(Computadores end);
+	
+	public boolean removerComputadores(Computadores end);
+	
+	public Computadores buscarComputadores(Computadores end);
+
+}
