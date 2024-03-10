840cbc5d3faef28f24138272e9956f11123fe343
Gustavo <gustavo.hl21@aluno.ifsc.edu.br>
2024-03-10 19:23:19 -0300

    Commit DAO4

--- santareserva/src/main/java/controle/ServicosDAO.java
+++ santareserva/src/main/java/controle/ServicosDAO.java
@@ -8,23 +8,21 @@ import java.util.ArrayList;
 
 import modelo.Servicos;
 
-public class ServicosDAO implements IServicosDAO {
-	
+public class ServicosDAO implements IServicosDAO{
+
 	private static ServicosDAO instancia;
 	
-	private ServicosDAO() {}
+	private ServicosDAO () {}
 	
-	public static ServicosDAO getInstancia() {
-		
-		if(instancia == null) {
+	public static ServicosDAO getInstancia () {
+		if(instancia == null ) {
 			instancia = new ServicosDAO();
 		}
 		return instancia;
 	}
 	
 	public int InserirServicos(Servicos servico) {
-		
-		String SQL = "INSERT INTO Servicos (ID_servico, Nome_servico, Preco_servico, Pag_Efetuado) VALUES (?, ?, ?, ?)";
+		String SQL = "INSERT INTO Servicos (ID_servico, Nome_servico, Preco_servico, PAG_Efetuado) VALUES (?,?,?,?)";
 		
 		Conexao con = Conexao.getInstancia();
 		Connection conBD = con.conectar();
@@ -33,21 +31,20 @@ public class ServicosDAO implements IServicosDAO {
 			PreparedStatement ps = conBD.prepareStatement(SQL);
 			
 			ps.setInt(1, servico.getID_servico());
-			ps.setString(2, servico.getNome_servico());
-			ps.setFloat(3, servico.getPreco_servico());
+			ps.setString(2,servico.getNome_servico());
+			ps.setFloat(3,servico.getPreco_servico());
 			ps.setBoolean(4, servico.getPag_Efetuado());
 			
 			return ps.executeUpdate();
-			
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
 			con.fecharConexao();
 		}
-			
 		return 0;
-	}
 		
+	}
+	
 	public ArrayList<Servicos> listarServicos() {
 		
 		ArrayList<Servicos> servicos = new ArrayList<Servicos>();
@@ -58,8 +55,8 @@ public class ServicosDAO implements IServicosDAO {
 		Connection conBD = con.conectar();
 		
 		try {
-			PreparedStatement ps= conBD.prepareStatement(SQL);
 			
+			PreparedStatement ps = conBD.prepareStatement(SQL);
 			ResultSet rs = ps.executeQuery();
 			
 			while(rs.next()) {
@@ -68,7 +65,7 @@ public class ServicosDAO implements IServicosDAO {
 				
 				Integer ID_servico = rs.getInt("ID_servico");
 				String nome_servico = rs.getString("Nome_servico");
-				Float preco_servico = rs.getFloat("Preco_servico");
+				Float preco_servico	= rs.getFloat("Preco_servico");
 				Boolean pag_efetuado = rs.getBoolean("Pag_Efetuado");
 				
 				servico.setID_servico(ID_servico);
@@ -77,54 +74,53 @@ public class ServicosDAO implements IServicosDAO {
 				servico.setPag_Efetuado(pag_efetuado);
 				
 				servicos.add(servico);
-				
-			}
+				}
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
 			con.fecharConexao();
 		}
-			
 		return servicos;
-	}
 		
+	}
+	
 	public boolean atualizarServicos(Servicos end) {
-
-				//Comando SQL a ser executado
-				String SQL = "UPDATE Servicos Set Pag_Efetuado = ? WHERE ID_servico = ?";
-				
-				// Abre a conexao e cria a "ponte de conexao" com MYSQL
-				Conexao con = Conexao.getInstancia();
-				Connection conBD = con.conectar();
-				
-				int retorno = 0;
-				
-				try {
-					PreparedStatement ps = conBD.prepareStatement(SQL);
-					
-					ps.setBoolean(1, end.getPag_Efetuado());
-					ps.setInt(2, end.getID_servico());
-					
-					retorno = ps.executeUpdate();
-					
-				} catch (SQLException e) {
-					e.printStackTrace();
-				} finally {
-					con.fecharConexao();
-				}
-				
-				return (retorno == 0 ? false : true);
-	}
-		
-	public boolean removerServicos(Servicos end) {
 			
+		String SQL = "UPDATE Servicos SET Pag_efetuado = ? WHERE ID_Servico = ?";
+		
+		Conexao con = Conexao.getInstancia();
+		Connection conBD = con.conectar();
+		
+		int retorno = 0;
+		
+		try {
+			PreparedStatement ps = conBD.prepareStatement(SQL);
+			
+			ps.setBoolean(1,end.getPag_Efetuado());
+			ps.setInt(2, end.getID_servico());
+			
+			retorno = ps.executeUpdate();
+			
+		} catch (SQLException e ) {
+			e.printStackTrace();
+		} finally {
+			con.fecharConexao();
+		}
+		
+		return (retorno == 0 ? false : true);
+		
+		
+		
+	}
+	
+	public boolean removerServicos(Servicos end ) {
 		return false;
 	}
 	
 	public Servicos buscarServicos(Servicos end) {
-			
+		
 		return null;
 	}
-
+	
 }
