c61dee9a0214ddd7fe27fd23731db8ac6af20306
MHBranco <murilo.b08@aluno.ifsc.edu.br>
2024-03-11 12:58:36 -0300

    ProtótipoTelaRegistro

--- santareserva/src/main/java/visao/TelaCadastro.java
+++ santareserva/src/main/java/visao/TelaCadastro.java
@@ -5,10 +5,23 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JLabel;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.ImageIcon;
 
 public class TelaCadastro extends JFrame {
 
-	private JPanel contentPane;
+	private JPanel Tela;
+	private JTextField txtUser;
+	private JTextField txtSobrenome;
+	private JTextField txtNascimento;
+	private JTextField txtTelefone;
+	private JTextField txtEmail;
+	private JTextField txtSenha;
+	private JTextField txtNome;
 
 	/**
 	 * Launch the application.
@@ -30,12 +43,89 @@ public class TelaCadastro extends JFrame {
 	 * Create the frame.
 	 */
 	public TelaCadastro() {
+		setTitle("Bem-vindo à Santa Reserva");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setBounds(100, 100, 1920, 1080);
+		Tela = new JPanel();
+		Tela.setBackground(new Color(238, 238, 238));
+		Tela.setBorder(new EmptyBorder(5, 5, 5, 5));
 
-		setContentPane(contentPane);
+		setContentPane(Tela);
+		Tela.setLayout(new MigLayout("", "[][][][][][][][][][][][][][][grow]", "[grow][][][][][60px]"));
+		
+		JPanel PainelCadastro = new JPanel();
+		Tela.add(PainelCadastro, "cell 14 0 1 6,alignx center,growy");
+		PainelCadastro.setLayout(new MigLayout("", "[195px,grow]", "[42px][][][40px][][40px][][40px][][40px][][40px][][40px][][40px][][][][][][grow][]"));
+		
+		JLabel lblRegistro = new JLabel("Registrar-se");
+		lblRegistro.setFont(new Font("Arial", Font.PLAIN, 36));
+		PainelCadastro.add(lblRegistro, "cell 0 0,alignx center,aligny top");
+		
+		JLabel lblUser = new JLabel("Nome de Usuário");
+		lblUser.setFont(new Font("Arial", Font.PLAIN, 32));
+		PainelCadastro.add(lblUser, "cell 0 2");
+		
+		txtUser = new JTextField();
+		txtUser.setFont(new Font("Arial", Font.PLAIN, 32));
+		PainelCadastro.add(txtUser, "cell 0 3,grow");
+		txtUser.setColumns(10);
+		
+		JLabel lblNome = new JLabel("Nome");
+		lblNome.setFont(new Font("Arial", Font.PLAIN, 32));
+		PainelCadastro.add(lblNome, "cell 0 4");
+		
+		txtNome = new JTextField();
+		PainelCadastro.add(txtNome, "cell 0 5,grow");
+		txtNome.setColumns(10);
+		
+		JLabel lblSobrenome = new JLabel("Sobrenome");
+		lblSobrenome.setFont(new Font("Arial", Font.PLAIN, 32));
+		PainelCadastro.add(lblSobrenome, "cell 0 6");
+		
+		txtSobrenome = new JTextField();
+		txtSobrenome.setFont(new Font("Arial", Font.PLAIN, 32));
+		PainelCadastro.add(txtSobrenome, "cell 0 7,grow");
+		txtSobrenome.setColumns(10);
+		
+		JLabel lblNascimento = new JLabel("Data de nascimento");
+		lblNascimento.setFont(new Font("Arial", Font.PLAIN, 32));
+		PainelCadastro.add(lblNascimento, "cell 0 8");
+		
+		txtNascimento = new JTextField();
+		PainelCadastro.add(txtNascimento, "cell 0 9,grow");
+		txtNascimento.setColumns(10);
+		
+		JLabel lblTelefone = new JLabel("Telefone");
+		lblTelefone.setFont(new Font("Arial", Font.PLAIN, 32));
+		PainelCadastro.add(lblTelefone, "cell 0 10");
+		
+		txtTelefone = new JTextField();
+		PainelCadastro.add(txtTelefone, "cell 0 11,grow");
+		txtTelefone.setColumns(10);
+		
+		JLabel lblEmail = new JLabel("Email");
+		lblEmail.setFont(new Font("Arial", Font.PLAIN, 32));
+		PainelCadastro.add(lblEmail, "cell 0 12");
+		
+		txtEmail = new JTextField();
+		PainelCadastro.add(txtEmail, "cell 0 13,grow");
+		txtEmail.setColumns(10);
+		
+		JLabel lblSenha = new JLabel("Senha");
+		lblSenha.setFont(new Font("Arial", Font.PLAIN, 32));
+		PainelCadastro.add(lblSenha, "cell 0 14");
+		
+		txtSenha = new JTextField();
+		PainelCadastro.add(txtSenha, "cell 0 15,grow");
+		txtSenha.setColumns(10);
+		
+		JLabel BTNRegistrar = new JLabel("");
+		BTNRegistrar.setIcon(new ImageIcon("C:\\Users\\Aluno\\Desktop\\PI-SantaReserva\\santareserva\\src\\main\\resources\\Botões\\BTN Registrar.png"));
+		PainelCadastro.add(BTNRegistrar, "cell 0 18");
+		
+		JLabel lblJapossui = new JLabel("Já possui registro? Faça o Login");
+		lblJapossui.setFont(new Font("Arial", Font.PLAIN, 20));
+		PainelCadastro.add(lblJapossui, "cell 0 22,alignx center,aligny bottom");
 	}
 
 }
--- santareserva/src/main/java/visao/TelaPerfil.java
+++ santareserva/src/main/java/visao/TelaPerfil.java
@@ -14,16 +14,16 @@ import java.awt.Font;
 
 public class TelaPerfil extends JFrame {
 
-	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
-	private JTextField textField_5;
-	private JTextField textField_6;
-	private JTextField textField_7;
-	private JTextField textField_8;
+	private JPanel PainelGeral;
+	private JTextField txtRua;
+	private JTextField txtUsuario;
+	private JTextField txtCep;
+	private JTextField txtBairro;
+	private JTextField txtSenha;
+	private JTextField txtCidade;
+	private JTextField txtEmail;
+	private JTextField txtVerSenha;
+	private JTextField txtConfSenha;
 
 	/**
 	 * Launch the application.
@@ -45,125 +45,124 @@ public class TelaPerfil extends JFrame {
 	 * Create the frame.
 	 */
 	public TelaPerfil() {
+		setFont(new Font("Arial", Font.PLAIN, 18));
+		setTitle("Perfil");
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(0, 0, 1920, 1080);
-		contentPane = new JPanel();
-		contentPane.setBackground(new Color(255, 255, 255));
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		PainelGeral = new JPanel();
+		PainelGeral.setBackground(new Color(255, 255, 255));
+		PainelGeral.setBorder(new EmptyBorder(5, 5, 5, 5));
 
-		setContentPane(contentPane);
-		contentPane.setLayout(new MigLayout("", "[][][210.00,grow 25][][39.00][21.00][1487.00,grow 25][grow][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][]", "[][][40.00][grow][][][][][][][][][][][][][][][][][][]"));
+		setContentPane(PainelGeral);
+		PainelGeral.setLayout(new MigLayout("", "[][][210.00,grow 25][][39.00][21.00][1487.00,grow 25][grow][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][]", "[][][40.00][grow][][][][][][][][][][][][][][][][][][]"));
 		
 		JLabel lblLogo = new JLabel("");
 		lblLogo.setIcon(new ImageIcon("C:\\Users\\Aluno\\Desktop\\PI-SantaReserva\\santareserva\\src\\main\\resources\\BackgroundsPI\\Rectangle 3 barra s home.png"));
-		contentPane.add(lblLogo, "cell 0 0 8 1");
+		PainelGeral.add(lblLogo, "cell 0 0 8 1");
 		
 		JLabel lblNewLabel_10 = new JLabel("");
 		lblNewLabel_10.setIcon(new ImageIcon("C:\\Users\\Aluno\\Desktop\\PI-SantaReserva\\santareserva\\src\\main\\resources\\BackgroundsPI\\Line 1.png"));
-		contentPane.add(lblNewLabel_10, "cell 0 1 8 1,aligny top");
+		PainelGeral.add(lblNewLabel_10, "cell 0 1 8 1,aligny top");
 		
 		JLabel lblNewLabel_11 = new JLabel("");
-		contentPane.add(lblNewLabel_11, "cell 7 2");
+		PainelGeral.add(lblNewLabel_11, "cell 7 2");
 		
 		JLabel lblNewLabel_12 = new JLabel("");
-		contentPane.add(lblNewLabel_12, "flowx,cell 2 5");
+		PainelGeral.add(lblNewLabel_12, "flowx,cell 2 5");
 		
-		JPanel panel = new JPanel();
-		panel.setBackground(new Color(238, 238, 238));
-		contentPane.add(panel, "cell 6 3 1 6,grow");
-		panel.setLayout(new MigLayout("", "[grow][178.00][142.00][::200.00px][69.00][173.00][46px,grow]", "[66px][][][][40][][40][][40][46.00][][40][][40][169.00][20][5px:n][40][15][][5px:n][40][131.00][28.00]"));
+		JPanel PainelAlteracao = new JPanel();
+		PainelAlteracao.setBackground(new Color(238, 238, 238));
+		PainelGeral.add(PainelAlteracao, "cell 6 3 1 6,grow");
+		PainelAlteracao.setLayout(new MigLayout("", "[grow][178.00][142.00][::200.00px][69.00][173.00][46px,grow]", "[66px][][][][40][][40][][40][46.00][][40][][40][169.00][20][5px:n][40][15][][5px:n][40][131.00][28.00]"));
 		
 		JLabel lblPerfil = new JLabel("");
-		panel.add(lblPerfil, "cell 3 0,alignx center,aligny center");
+		PainelAlteracao.add(lblPerfil, "cell 3 0,alignx center,aligny center");
 		lblPerfil.setIcon(new ImageIcon("C:\\Users\\Aluno\\Desktop\\PI-SantaReserva\\santareserva\\src\\main\\resources\\Icones\\IconeUser.png"));
 		
-		JLabel lblNewLabel_14 = new JLabel("User");
-		lblNewLabel_14.setFont(new Font("Arial", Font.PLAIN, 18));
-		panel.add(lblNewLabel_14, "cell 3 1,alignx center");
+		JLabel lblUser = new JLabel("User");
+		lblUser.setFont(new Font("Arial", Font.PLAIN, 18));
+		PainelAlteracao.add(lblUser, "cell 3 1,alignx center");
 		
-		JLabel lblNewLabel = new JLabel("Alterar Rua");
-		lblNewLabel.setFont(new Font("Arial", Font.PLAIN, 18));
-		panel.add(lblNewLabel, "cell 1 3,alignx left,aligny center");
+		JLabel lblAltRua = new JLabel("Alterar Rua");
+		lblAltRua.setFont(new Font("Arial", Font.PLAIN, 18));
+		PainelAlteracao.add(lblAltRua, "cell 1 3,alignx left,aligny center");
 		
-		JLabel lblNewLabel_1 = new JLabel("Alterar Nome de Usuário");
-		lblNewLabel_1.setFont(new Font("Arial", Font.PLAIN, 18));
-		panel.add(lblNewLabel_1, "cell 4 3 2 1");
+		JLabel lblAltusuario = new JLabel("Alterar Nome de Usuário");
+		lblAltusuario.setFont(new Font("Arial", Font.PLAIN, 18));
+		PainelAlteracao.add(lblAltusuario, "cell 4 3 2 1");
 		
-		textField = new JTextField();
-		panel.add(textField, "cell 1 4 2 1,grow");
-		textField.setColumns(10);
+		txtRua = new JTextField();
+		PainelAlteracao.add(txtRua, "cell 1 4 2 1,grow");
+		txtRua.setColumns(10);
 		
-		textField_1 = new JTextField();
-		panel.add(textField_1, "cell 4 4 2 1,grow");
-		textField_1.setColumns(10);
+		txtUsuario = new JTextField();
+		PainelAlteracao.add(txtUsuario, "cell 4 4 2 1,grow");
+		txtUsuario.setColumns(10);
 		
-		JLabel lblNewLabel_2 = new JLabel("Alterar Bairro");
-		lblNewLabel_2.setFont(new Font("Arial", Font.PLAIN, 18));
-		panel.add(lblNewLabel_2, "cell 1 5");
+		JLabel lblAltBairro = new JLabel("Alterar Bairro");
+		lblAltBairro.setFont(new Font("Arial", Font.PLAIN, 18));
+		PainelAlteracao.add(lblAltBairro, "cell 1 5");
 		
-		JLabel lblNewLabel_3 = new JLabel("Alterar Senha");
-		lblNewLabel_3.setFont(new Font("Arial", Font.PLAIN, 18));
-		panel.add(lblNewLabel_3, "cell 4 5 2 1");
+		JLabel lblAltSenha = new JLabel("Alterar Senha");
+		lblAltSenha.setFont(new Font("Arial", Font.PLAIN, 18));
+		PainelAlteracao.add(lblAltSenha, "cell 4 5 2 1");
 		
-		textField_3 = new JTextField();
-		panel.add(textField_3, "cell 1 6 2 1,grow");
-		textField_3.setColumns(10);
+		txtBairro = new JTextField();
+		PainelAlteracao.add(txtBairro, "cell 1 6 2 1,grow");
+		txtBairro.setColumns(10);
 		
-		textField_4 = new JTextField();
-		panel.add(textField_4, "cell 4 6 2 1,grow");
-		textField_4.setColumns(10);
+		txtSenha = new JTextField();
+		PainelAlteracao.add(txtSenha, "cell 4 6 2 1,grow");
+		txtSenha.setColumns(10);
 		
-		JLabel lblNewLabel_4 = new JLabel("Alterar Cidade");
-		lblNewLabel_4.setFont(new Font("Arial", Font.PLAIN, 18));
-		panel.add(lblNewLabel_4, "cell 1 7");
+		JLabel lblAltCidade = new JLabel("Alterar Cidade");
+		lblAltCidade.setFont(new Font("Arial", Font.PLAIN, 18));
+		PainelAlteracao.add(lblAltCidade, "cell 1 7");
 		
-		JLabel lblNewLabel_5 = new JLabel("Alterar Email");
-		lblNewLabel_5.setFont(new Font("Arial", Font.PLAIN, 18));
-		panel.add(lblNewLabel_5, "cell 4 7 2 1");
+		JLabel lblAltEmail = new JLabel("Alterar Email");
+		lblAltEmail.setFont(new Font("Arial", Font.PLAIN, 18));
+		PainelAlteracao.add(lblAltEmail, "cell 4 7 2 1");
 		
-		textField_5 = new JTextField();
-		panel.add(textField_5, "cell 1 8 2 1,grow");
-		textField_5.setColumns(10);
+		txtCidade = new JTextField();
+		PainelAlteracao.add(txtCidade, "cell 1 8 2 1,grow");
+		txtCidade.setColumns(10);
 		
-		textField_6 = new JTextField();
-		panel.add(textField_6, "cell 4 8 2 1,grow");
-		textField_6.setColumns(10);
+		txtEmail = new JTextField();
+		PainelAlteracao.add(txtEmail, "cell 4 8 2 1,grow");
+		txtEmail.setColumns(10);
 		
 		JLabel lblNewLabel_6 = new JLabel("Alterar CEP");
 		lblNewLabel_6.setFont(new Font("Arial", Font.PLAIN, 18));
-		panel.add(lblNewLabel_6, "cell 2 12 3 1,growy");
+		PainelAlteracao.add(lblNewLabel_6, "cell 2 12 3 1,growy");
 		
-		textField_2 = new JTextField();
-		panel.add(textField_2, "cell 2 13 3 1,grow");
-		textField_2.setColumns(10);
+		txtCep = new JTextField();
+		PainelAlteracao.add(txtCep, "cell 2 13 3 1,grow");
+		txtCep.setColumns(10);
 		
 		JLabel lblNewLabel_7 = new JLabel("Senha");
 		lblNewLabel_7.setFont(new Font("Arial", Font.PLAIN, 18));
-		panel.add(lblNewLabel_7, "cell 2 15 3 1,alignx left");
+		PainelAlteracao.add(lblNewLabel_7, "cell 2 15 3 1,alignx left");
 		
-		textField_7 = new JTextField();
-		panel.add(textField_7, "cell 2 17 3 1,grow");
-		textField_7.setColumns(10);
+		txtVerSenha = new JTextField();
+		PainelAlteracao.add(txtVerSenha, "cell 2 17 3 1,grow");
+		txtVerSenha.setColumns(10);
 		
 		JLabel lblNewLabel_8 = new JLabel("Confirmar Senha");
 		lblNewLabel_8.setFont(new Font("Arial", Font.PLAIN, 18));
-		panel.add(lblNewLabel_8, "cell 2 19 3 1,alignx left");
+		PainelAlteracao.add(lblNewLabel_8, "cell 2 19 3 1,alignx left");
 		
-		textField_8 = new JTextField();
-		panel.add(textField_8, "cell 2 21 3 1,grow");
-		textField_8.setColumns(10);
+		txtConfSenha = new JTextField();
+		PainelAlteracao.add(txtConfSenha, "cell 2 21 3 1,grow");
+		txtConfSenha.setColumns(10);
 		
-		JLabel lblNewLabel_13 = new JLabel("");
-		lblNewLabel_13.setIcon(new ImageIcon("C:\\Users\\Aluno\\Desktop\\PI-SantaReserva\\santareserva\\src\\main\\resources\\Botões\\BTN Aplicar Atualizações.png"));
-		panel.add(lblNewLabel_13, "cell 2 22 3 1,alignx center,aligny bottom");
+		JLabel BtnAtualizarInfo = new JLabel("");
+		BtnAtualizarInfo.setIcon(new ImageIcon("C:\\Users\\Aluno\\Desktop\\PI-SantaReserva\\santareserva\\src\\main\\resources\\Botões\\BTN Aplicar Atualizações.png"));
+		PainelAlteracao.add(BtnAtualizarInfo, "cell 2 22 3 1,alignx center,aligny bottom");
 		
-		JLabel lblNewLabel_9 = new JLabel("");
-		contentPane.add(lblNewLabel_9, "cell 0 9");
-		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBackground(new Color(238, 238, 238));
-		contentPane.add(panel_1, "cell 1 2 2 20,grow");
+		JPanel ColunaIcones = new JPanel();
+		ColunaIcones.setBackground(new Color(238, 238, 238));
+		PainelGeral.add(ColunaIcones, "cell 1 2 2 20,grow");
 	}
 
 }
