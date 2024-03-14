1cf497d9a1feb28e7638246989480b6122c630f0
Gustavo <gustavo.hl21@aluno.ifsc.edu.br>
2024-03-14 07:19:26 -0300

    CorreçãoConexão

--- santareserva/src/main/java/controle/Conexao.java
+++ santareserva/src/main/java/controle/Conexao.java
@@ -4,55 +4,52 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class Conexao { //Connection
-	
+public class Conexao { // Connection
+
 	private static final String USERNAME = "root";
-<<<<<<< Updated upstream
-	private static final String SENHA = "gusta2023157c.c";
-=======
 	private static final String SENHA = "aluno";
->>>>>>> Stashed changes
 	private static final String BD = "mydb";
 	private Connection con; // jdbc
-	private static  Conexao instancia; // singleton
-	
-	private Conexao() {} // construtor privado
-	
+	private static Conexao instancia; // singleton
+
+	private Conexao() {
+	} // construtor privado
+
 	/**
 	 * Metodo singleton
 	 * 
 	 * @return
 	 */
-	
+
 	public static Conexao getInstancia() {
-		if(instancia == null) {
+		if (instancia == null) {
 			instancia = new Conexao();
 		}
 		return instancia;
 	}
-	
+
 	/**
 	 * Método que faz conexao com o MySQL
 	 * 
 	 * @return
 	 */
-	
+
 	public Connection conectar() {
 		try {
-			con = DriverManager.getConnection("jdbc:mysql://localhost/"+ BD + "?serverTimezone=UTC", USERNAME, SENHA);
+			con = DriverManager.getConnection("jdbc:mysql://localhost/" + BD + "?serverTimezone=UTC", USERNAME, SENHA);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 		return con;
 	}
-	
+
 	/**
 	 * Fecha a conexao com o MySQL
 	 * 
 	 * @return
 	 */
-	
+
 	public boolean fecharConexao() {
 		try {
 			con.close();
