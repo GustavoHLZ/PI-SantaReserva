5ad2fef5d1e4cb9af0f0f16ae15477f07ed9c3d5
Kauã <kaua.bm@aluno.ifsc.edu.br>
2024-04-01 14:59:32 -0300

    CommitTelaQuarto1.0

--- santareserva/src/main/java/visao/TelaQuartos.java
+++ santareserva/src/main/java/visao/TelaQuartos.java
@@ -1,15 +1,18 @@
 package visao;
 
+import java.awt.Color;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import net.miginfocom.swing.MigLayout;
+import java.awt.SystemColor;
 
 public class TelaQuartos extends JFrame {
 
 	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
+	private JPanel PainelGeral;
 
 	/**
 	 * Launch the application.
@@ -31,12 +34,29 @@ public class TelaQuartos extends JFrame {
 	 * Create the frame.
 	 */
 	public TelaQuartos() {
+		setTitle("Quartos");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setBounds(0, 0, 1920, 1080);
+		PainelGeral = new JPanel();
+		PainelGeral.setBackground(new Color(255, 255, 255));
+		PainelGeral.setBorder(new EmptyBorder(5, 5, 5, 5));
 
-		setContentPane(contentPane);
+		setContentPane(PainelGeral);
+		PainelGeral.setLayout(new MigLayout("", "[][grow][][][][][][][][][][grow][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][]", "[][][][][][grow][grow][][][][][][][][][][][][][][][][][][][][][][][][][][][][][]"));
+		
+		JPanel PainelTopo = new JPanel();
+		PainelTopo.setBackground(new Color(119, 165, 175));
+		PainelGeral.add(PainelTopo, "cell 0 0 66 5,grow");
+		
+		JPanel PainelIcones = new JPanel();
+		PainelIcones.setBackground(new Color(239, 239, 239));
+		PainelGeral.add(PainelIcones, "cell 1 5 9 31,grow");
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(239, 239, 239));
+		PainelGeral.add(panel, "cell 11 6 55 30,grow");
+		
+		
 	}
 
 }
