14afbb37a53f127e43d5123d3f7ba80210d3f81a
Kauã <kaua.bm@aluno.ifsc.edu.br>
2024-04-29 11:04:10 -0300

    Arrumar Telas pra pegar pelo BDD

--- santareserva/src/main/java/visao/TelaQuartos.java
+++ santareserva/src/main/java/visao/TelaQuartos.java
@@ -147,7 +147,7 @@ public class TelaQuartos extends JFrame {
 		lblNewLabel_5.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				TelaReservas c = new TelaReservas(hospede, null, quartoSelecionado, null, null);
+				TelaReservas c = new TelaReservas(hospede, null, quartoSelecionado, null, null, null);
 				c.setVisible(true);
 				dispose();
 			}
@@ -241,7 +241,7 @@ public class TelaQuartos extends JFrame {
 		lblNewLabel_20.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				TelaReservas telaReservas = new TelaReservas(hosplogado, usuariologado , quartoSelecionado, null, null);
+				TelaReservas telaReservas = new TelaReservas(hosplogado, usuariologado , quartoSelecionado, listaQuartos, null, null);
 		        telaReservas.setVisible(true);
 				
 				String checkin = textCheckIn.getText();
