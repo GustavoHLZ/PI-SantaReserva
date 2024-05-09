6e0315275911caded50597c0052f8f6c0df27d21
Luigi <vicenzo.schramm@gmail.com>
2024-05-09 11:38:53 -0300

    CommitAjustesIComputadoresDAO

--- santareserva/src/main/java/controle/IComputadoresDAO.java
+++ santareserva/src/main/java/controle/IComputadoresDAO.java
@@ -1,23 +1,5 @@
 package controle; 
  
-import java.util.ArrayList; 
- 
-import modelo.Computadores; 
- 
-public interface IComputadoresDAO { 
- 
-	public int inserirComputadores(Computadores end); 
-	 
-	public ArrayList<Computadores> listarComputadores(); 
-	 
-	public boolean atualizarComputadores(Computadores end); 
-	 
-	public Computadores removerComputadores(Computadores end); 
-	 
-	public Computadores buscarComputadores(Computadores end); 
- 
-} 
-
 import java.util.ArrayList;
 
 import modelo.Computadores;
